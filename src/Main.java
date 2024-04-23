0d08d1d1792a14cc8dd2c2e0a965dc50295642ca
drocherg <drocher@elpuig.xeill.net>
2024-04-23 20:13:10 +0200

    Metodos puestos en el metodo menu

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         TUI tui = new TUI();
-            tui.menu();
-        int opcioEscollida = sc.nextInt();
-        while (opcioEscollida <= 3) {
-            //Sale del bucle cuando se escribe 4, si no vuleve a mostrar el menu
-            //Se escribe del 1 al 4 la opcion deseada
-            if (opcioEscollida == 1) {
-                System.out.println("Has seleccionat la opció 1: Nova partida.");
-                //Se vuelve a mostrar el menú
-                //Nueva partida;
+        tui.menu();
 
-            } else if (opcioEscollida == 2) {
-                System.out.println("Has seleccionat la opció 2: Carregar partida.");
-                //Se vuelve a mostrar el menú
-                //Carregar partida;
-
-            } else if (opcioEscollida == 3) {
-                System.out.println("Has seleccionat la opció 3: Configuració.");
-                //Se vuelve a mostrar el menú
-                //Configuración;
-
-            }
-            //Sale del menu
-            tui.menu();
-            opcioEscollida = sc.nextInt();
-        }
-        System.out.println("Sortint...");
     }
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TUI {
     private static void novaPartida(){
         System.out.println("1. Nova partida");
@@ -12,11 +15,39 @@ public class TUI {
         System.out.println("4. Sortir");
     }
     public static void menu(){
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
         System.out.println("Esculli una opció:");
         novaPartida();
         carregarPartida();
         configuracio();
         sortir();
 
+        int opcioEscollida = sc.nextInt();
+        while (opcioEscollida <= 4) {
+            //Sale del bucle cuando se escribe 4, si no vuleve a mostrar el menu
+            //Se escribe del 1 al 4 la opcion deseada
+            if (opcioEscollida == 1) {
+                System.out.println("Has seleccionat la opció 1: Nova partida.");
+                menu();
+                //Nueva partida;
+
+            } else if (opcioEscollida == 2) {
+                System.out.println("Has seleccionat la opció 2: Carregar partida.");
+                menu();
+                //Carregar partida;
+
+            } else if (opcioEscollida == 3) {
+                System.out.println("Has seleccionat la opció 3: Configuració.");
+                menu();
+                //Configuración;
+
+            } else if (opcioEscollida == 4) {
+                System.out.println("Sortint...");
+                break;
+            }
+            //Sale del menu
+        }
     }
 }
