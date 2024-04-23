c1d36a748b7553443df904a5b767245ca4190a68
drocherg <drocher@elpuig.xeill.net>
2024-04-23 17:37:32 +0200

    Metodos puestos en su sitio

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,6 @@ public class Main {
         int opcioEscollida = sc.nextInt();
 
     }
-    private static void novaPartida(){
-        System.out.println("1. Nova partida");
-    }
-    private static void carregarPartida(){
-        System.out.println("2. Carregar partida");
-    }
-    private static void configuracio(){
-        System.out.println("3. Configuració");
-    }
-    private static void sortir() {
-        System.out.println("4. Sortir");
-    }
-    private short menu(){
-        novaPartida();
-        carregarPartida();
-        configuracio();
-        sortir();
-        return menu();
-    }
+
+
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,4 +1,21 @@
 public class TUI {
-    Main main = new Main ();
-    main.menu;
+    private static void novaPartida(){
+        System.out.println("1. Nova partida");
+    }
+    private static void carregarPartida(){
+        System.out.println("2. Carregar partida");
+    }
+    private static void configuracio(){
+        System.out.println("3. Configuració");
+    }
+    private static void sortir() {
+        System.out.println("4. Sortir");
+    }
+    public short menu(){
+        novaPartida();
+        carregarPartida();
+        configuracio();
+        sortir();
+        return menu();
+    }
 }
