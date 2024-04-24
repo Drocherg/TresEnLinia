dc02795c01541c6c43efe022bba125124511a0d4
izguta2 <izguta@elpuig.xeill.net>
2024-04-24 19:08:01 +0200

    He cambiado el if por el switch y he comentado el while

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.useLocale(Locale.ENGLISH);
         TUI tui = new TUI();
         tui.menu();
 
