c4b2184e9df66199faecafae019f20b19dc59b03
drocherg <drocher@elpuig.xeill.net>
2024-04-24 19:47:47 +0200

    Clase Joc creada con sus metodos (Faltan las matrices)

--- src/Joc.java
+++ src/Joc.java
@@ -1,2 +1,25 @@
 public class Joc {
+    private char taulell;
+    private short torn;
+
+    public short getTorn() {
+        return torn;
+    }
+
+    public char getTaulell() {
+        return taulell;
+    }
+
+    novaPartida();
+
+    public static void jugar(){
+        short fila;
+        short columna;
+        return;
+    }
+    public boolean jugadaGuanyadora(){
+        short fila;
+        short columna;
+        return jugadaGuanyadora();
+    }
 }
