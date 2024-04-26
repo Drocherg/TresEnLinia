f70939645fd9f890f77a65ab5b1505beaf5aad35
izguta2 <izguta@elpuig.xeill.net>
2024-04-26 17:33:24 +0200

    He puesto los metodos en main y tui

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-import jdk.jshell.spi.ExecutionControl;
 
 public class Main {
     public static void main(String[] args) {
 
     }
-    private static void novaPartida(){
-        throw new ExecutionControl.NotImplementedException();
-
+    private static String novaPartida(){
+        return novaPartida();
     }
-    private static void carregarPartida(){
-        throw new ExecutionControl.NotImplementedException();
+    private static String carregarPartida(){
+        return carregarPartida();
     }
-    private static void configuracio(){
-        throw new ExecutionControl.NotImplementedException();
+    private static String configuracio(){
+        return configuracio();
     }
-    private void sortir() {
+    private static String sortir() {
+        return sortir();
     }
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 
 public class TUI {
-    Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
 
     public static void mostrarMenu() {
         System.out.println("1. Nova partida");
         System.out.println("2. Carregar partida");
         System.out.println("3. Configuració");
         System.out.println("4. Sortir");
+    }
+    public static void mostrarTaulell(char taulell, short torn){
+        char taulell1 = taulell;
+        short torn1 = torn;
+    }
+    public static short recollirJugada(){
+        return 0;
+    }
+    public static void finDePartida(short guanyador){
     }
 }
