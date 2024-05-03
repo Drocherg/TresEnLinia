119508b1dba193a2b2e85ad98bc45c88ddcf3769
drocherg <drocher@elpuig.xeill.net>
2024-05-03 17:52:54 +0200

    Comenzando pruebas unitarias v2

--- src/test/java/JocTest.java
+++ src/test/java/JocTest.java
@@ -1,11 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*import org.junit.jupiter.api.Assertions;
 
 class JocTest {
 
     @org.junit.jupiter.api.Test
-    void novaPartida() {
-        assertEquals(' ', TUI.joc.getTaulell()[0][0]); // Comprobamos la primera casilla
-        assertEquals(' ', TUI.joc.getTaulell()[0][1]); // Comprobamos la segunda casilla (y así sucesivamente)
-        assertEquals(1, TUI.joc.getTorn()); // Verificamos que sea el turno del jugador 1
+    void creacion () {
     }
-}
+
+    @org.junit.jupiter.api.Test
+    void primerturno() {
+        int x = 1;
+        int y = 1;
+        char[][] tc = new char[3][3];
+        tc[x][y] = 'x';
+        Joc joc = new Joc();
+        Assertions.assertArrayEquals(tc, joc.getTaulell());
+    }
+}*/
